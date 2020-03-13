f9d6083c6108f66a835ec845fe114b650a10c7e7
Alex Forrence <alex.forrence@gmail.com>
2020-03-13 14:44:16 -0400

    enable vsync

--- examples/jamboree.py
+++ examples/jamboree.py
@@ -16,7 +16,7 @@ from mglg.graphics.text2d import Text2D, DynamicText2D
 # import gamemode as gm
 
 if __name__ == '__main__':
-    win = Win(vsync=0, screen=0)
+    win = Win(vsync=1, screen=0)
     win.ctx.line_width = 3.0
 
     sqr = Square(win, scale=(0.15, 0.1), fill_color=(0.7, 0.9, 0.2, 1), rotation=45)
