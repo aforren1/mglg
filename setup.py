a92cd53bb323dfd3d8dcfa352749ab26f5dd3bff
Alex Forrence <alex.forrence@gmail.com>
2020-05-14 18:26:22 -0400

    bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="mglg",
-    version="0.1.1a2",
+    version="0.1.1a3",
     install_requires=requirements,
     author="Alex Forrence",
     author_email="alex.forrence@gmail.com",
