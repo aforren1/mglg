ff70b2ecde03c2b142565bf817ac508a380b371f
Alex Forrence <alex.forrence@gmail.com>
2020-06-25 11:36:04 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="mglg",
-    version="0.2.12",
+    version="0.2.13",
     install_requires=requirements,
     extras_require={'freetype': ['freetype-py']},
     author="Alex Forrence",
