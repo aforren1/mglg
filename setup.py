407b44042f5a0662244635b96add3a02a9af5902
Alex Forrence <alex.forrence@gmail.com>
2020-08-05 12:29:10 -0400

    0.2.15

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="mglg",
-    version="0.2.15a1",
+    version="0.2.15",
     install_requires=requirements,
     extras_require={'freetype': ['freetype-py']},
     author="Alex Forrence",
